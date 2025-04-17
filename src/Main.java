e666941eb7552d191017204bab5c739574e779cc
JSpezan <juliaspezani@id.uff.br>
2025-04-17 12:58:45 -0300

    Adicionando demais arquivos para o  modelo do ERENO na criação do arquivo pcap

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 public class Main {
+    //Main java criado automaticamente, não foi setado ainda para criar/chamar os demais arquivos/classes
+    //Configurar depois
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
